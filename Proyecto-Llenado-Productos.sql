b076aebe3c6454a5b751e4668fbf5bd2eb40c300
Lalopa <16031227@itcelaya.edu.mx>
2019-09-06 19:07:30 -0500

    sexto commit

--- Proyecto-Llenado-Productos.sql
+++ Proyecto-Llenado-Productos.sql
@@ -1,40 +1,40 @@
 use proyecto;
 
-insert into Sombrero (cveSombrero, Material, Estilo, Color, Nombre)
+insert into sombrero (cveSombrero, Material, Estilo, Color, Nombre)
 values ('SOM_1','Tela','Formal','Azul','Sombrero Negro de Tela'),
 		('SOM_2','Gamusa','De Campo','Negro','Sombrero Tejano de Gamusa');
 
 
-insert into Reloj (cveReloj, Material, Tipo, Color, Nombre)
+insert into seloj (cveReloj, Material, Tipo, Color, Nombre)
 values ('REL_1','Cuero','Analogico','Negro','Reloj Analogico Negro Casual'),
 		('REL_2','Caucho','Digital','Negro','Reloj Digital Negro Deportivo');
 
-insert into Zapatos (cveZapato, Material, Ocasion,  Color,Nombre)
-values ('ZAP_1', 'Piel', 'Casual', 'Cafe', 'Zapatos Casuales Cáfez duro'),
+insert into zapatos (cveZapato, Material, Ocasion,  Color,Nombre)
+values ('ZAP_1', 'Piel', 'Casual', 'Cafe', 'Zapatos Casuales Cï¿½fez duro'),
 		('ZAP_2', 'Piel', 'Formal', 'Blanco', 'Zapatos Blancos de Tacon');
 
-insert into Lentes (cveLentes,Tipo, Color, Nombre)
+insert into lentes (cveLentes,Tipo, Color, Nombre)
 values ('LEN_1', 'De vista', 'Negro', 'Lentes oftanmologicos redondos'),
 		('LEN_2', 'De sol', 'Negro', 'Lenses de sol mica Roja');
 
-insert into Corbata (cveCorbata, Material, Tipo, Color, Nombre)
+insert into corbata (cveCorbata, Material, Tipo, Color, Nombre)
 values ('COR_!', 'Poliester', 'Corbata', 'Rojo', 'Corbata Lisa Roja de Poliester'),
 		('COR_2', 'Poliester', 'Mono', 'Azul', 'Mono Azul con Lineas');
 
-insert into Playera (cvePlayera, Tipo, Color, Nombre)
+insert into playera (cvePlayera, Tipo, Color, Nombre)
 values ('PLA_1', 'Playera', 'Rojo', 'Playera estampada Roja oscura'),
 		('PLA_2', 'Polo', 'Rojo', 'Polo Roja Lisa Brillante');
 
-insert into Pantalon (cvePantalon,Material,Color,Nombre)
+insert into pantalon (cvePantalon,Material,Color,Nombre)
 values ('PAN_1', 'Mezclilla', 'Azul', 'Pantalon de Mezclilla Claro'),
 		('PAN_2', 'Tela', 'Negro', 'Pantalon Casual Negro');
 
-insert into Cinturon (CveCinturon,Color,Nombre)
+insert into cinturon (CveCinturon,Color,Nombre)
 values ('CIN_1', 'Azul','Cinturon Trenzado Azul y Cafe'),
 	('CIN_2', 'Cafe', 'Cinturon Cafe Formal');
 
 		
-insert into Producto (CveProducto,Cantidad, Precio)
+insert into producto (CveProducto,Cantidad, Precio)
 values ('SOM_1',10, 180.3),
 		('SOM_2', 5, 299.9),
 		('REL_1', 4,5000.0),
@@ -53,6 +53,6 @@ values ('SOM_1',10, 180.3),
 		('CIN_2', 3,299.2);
 
 
-insert into Ofertas (CveOfertas, Descuento, Descripcion)
+insert into ofertas (CveOfertas, Descuento, Descripcion)
 values ('PROMO100', 33, 'Promosion de 30% en tu primera compra'),
 	   ('OFERTONBD', 95, 'Oferta valida solo para el profe de BD');
